5c415ea6cc12b863a158e31d3acd694b40e5b252
planetzsky <43303939+planetzsky@users.noreply.github.com>
2018-11-07 06:22:26 +0000

    updated index,js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const config = require("./config.json");
-const token = require("./config.json")
 const Discord = require("discord.js");
-
+const prefix = config.prefix;
 const bot = new Discord.Client({disableEveryone: true});
+const token = require("./config.json");
+
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
+    console.log(`${bot.user.username} is online!`);
+  
+    bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+  });
+  bot.on("message", async () => {
+if(!message.content.startsWith === prefix) return;
+  
+    const msg = await message.channel.send('Poggers!');
 
-  bot.user.setActivity("how to shart without making a mess", {type: "WATCHING"});
-
-  //bot.user.setGame("on SourceCade!");
+    if(cmd === `${prefix}ping`){     
+msg.edit(`Pong! I took \`${msg.createdTimestamp - message.createdTimestamp}ms\``);    
+    }  
 });
 
-
+bot.login(token);
